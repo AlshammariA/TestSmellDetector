fa2d5ba926d41021dae55d88d9e4258bbfb23fc2
Michael Hilton <michael.hilton@gmail.com>
2018-12-02 21:05:37 -0500

    added code to go through each commit in a dir, and find all files with Test annotation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import testsmell.AbstractSmell;
 import testsmell.ResultsWriter;
 import testsmell.TestFile;
 import testsmell.TestSmellDetector;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,21 +25,106 @@ public class Main {
             System.out.println("Please provide the file containing the paths to the collection of test files");
             return;
         }
+//        if(!args[0].isEmpty()){
+//            File inputFile = new File(args[0]);
+//            if(!inputFile.exists() || inputFile.isDirectory()) {
+//                System.out.println("Please provide a valid file containing the paths to the collection of test files");
+//                return;
+//            }
+//        }
         if(!args[0].isEmpty()){
             File inputFile = new File(args[0]);
-            if(!inputFile.exists() || inputFile.isDirectory()) {
-                System.out.println("Please provide a valid file containing the paths to the collection of test files");
-                return;
+            if(inputFile.exists() && inputFile.isDirectory()) {
+                System.out.println("Process Directory: " + inputFile.toString());
             }
         }
 
 
+        String workingDir = args[0];
+
+        getAllRevisions(workingDir);
+
+//        List<Path> testFiles = findTestFiles(workingDir);
+
+//        detectSmells(workingDir);
+
+        System.out.println("end");
+    }
+
+    private static void getAllRevisions(String workingDir) {
+        ArrayList<String> allRevisions = new ArrayList<>();
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        Repository repo = null;
+        Iterable<RevCommit> log = null;
+        Git git = null;
+        try {
+            repo = builder.setGitDir(new File(workingDir+"/.git")).setMustExist(true).build();
+            git = new Git(repo);
+            log = null;
+            log = git.log().call();
+        for (Iterator<RevCommit> iterator = log.iterator(); iterator.hasNext();) {
+            RevCommit rev = iterator.next();
+            System.out.println(rev.name());
+            allRevisions.add(rev.name());
+//            git.checkout().setName( rev.name() ).call();
+            //            logMessages.add(rev.getFullMessage());
+            }
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String rev: allRevisions) {
+            try {
+                git.checkout().setName( rev ).call();
+                System.out.println("Checked Out: "+ rev);
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+
+    }
+
+    //find all files with an @test annotation
+    private static List<Path> findTestFiles(String workingDir) {
+        List<Path> filesList = null;
+        try {
+            filesList = Files.walk(Paths.get(workingDir))
+                    .filter(p -> p.toString().endsWith(".java"))
+                    .filter(f -> {
+                        Charset charset = Charset.forName("US-ASCII");
+                        try (BufferedReader reader = Files.newBufferedReader(f, charset)) {
+                            String line = null;
+                            while ((line = reader.readLine()) != null) {
+//                                System.out.println(line);
+                                if(line.contains("@Test")){
+                                    return true;
+                                }
+                            }
+                        } catch (IOException x) {
+                            System.err.format("IOException: %s%n", x);
+                        }
+                    return false;})
+                .collect(Collectors.toList());
+//                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return filesList;
+    }
+
+    private static void detectSmells(String arg) throws IOException {
         TestSmellDetector testSmellDetector = TestSmellDetector.createTestSmellDetector();
 
         /*
           Read the input file and build the TestFile objects
          */
-        BufferedReader in = new BufferedReader(new FileReader(args[0]));
+        BufferedReader in = new BufferedReader(new FileReader(arg));
         String str;
 
         String[] lineItem;
@@ -103,8 +194,6 @@ public class Main {
             }
             resultsWriter.writeLine(columnValues);
         }
-
-        System.out.println("end");
     }
 
 
